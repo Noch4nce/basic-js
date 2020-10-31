ae98942333d8ca125dca81cc67d274de611fa843
@Noch4nce <m4rgera123@mail.ru>
2020-10-31 17:30:50 +0500

    feat: implement repeater, cipher

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,11 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    let { repeatTimes, separator, addition, additionRepeatTimes, additionSeparator } = options;
+    separator = separator || '+';
+    additionSeparator = additionSeparator || '|';
+    addition = (addition === null) ? 'null' : addition;
+
+    const newStr = new Array(additionRepeatTimes).fill(addition).join(additionSeparator);
+    return new Array(repeatTimes).fill(str + newStr).join(separator);
 };
-  
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,59 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(bool = true) {
+    this.bool = bool;
+  }
+
+  encrypt(str, key) {
+      if (typeof str === 'undefined' || typeof key === 'undefined') {
+        throw new Error();
+      }
+      let data = {
+          result: [],
+          index: 0,
+      };
+      for (let i = 0; i < str.length; i++) {
+          if (str[i].match(/[A-Za-z]/)) {
+              let codeCharSymbol = str[i].toUpperCase().charCodeAt();
+              data.result[i] = String.fromCharCode(
+                  ( codeCharSymbol + (key[data.index % key.length].toUpperCase().charCodeAt())
+                      - 65 * 2) % 26 + 65);
+              data.index++;
+          } else {
+              data.result[i] = str[i];
+          }
+      }
+      if (this.bool) {
+          return data.result.join('');
+      } else {
+          return data.result.reverse().join('');
+      }
+  }
+  decrypt(newStr, key) {
+      if (typeof newStr === 'undefined' || typeof key === 'undefined') {
+        throw new Error();
+      }
+      let data = {
+          result: [],
+          index: 0,
+      };
+      for (let i = 0; i < newStr.length; i++) {
+          if (newStr[i].match(/[A-Za-z]/)) {
+              let codeCharSymbol = newStr[i].toUpperCase().charCodeAt();
+              data.result[i] = String.fromCharCode(Math.abs(
+                  codeCharSymbol - (key[data.index % key.length].toUpperCase().charCodeAt())
+                  + 26) % 26 + 65);
+              data.index++;
+          } else {
+              data.result[i] = newStr[i];
+          }
+      }
+      if (this.bool) {
+          return data.result.join('');
+      } else {
+          return data.result.reverse().join('');
+      }
   }
 }
 
